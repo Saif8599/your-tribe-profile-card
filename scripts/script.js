c6b9ebfc4a58ad84b7143c8467e4434ce571703a
Saif <ninja_saif@hotmail.com>
2023-09-08 11:49:15 +0200

    Add profile-content with buttons
    Add profile content with working buttons to show each section

--- scripts/script.js
+++ scripts/script.js
@@ -1,6 +1,38 @@
-const expandButton = document.querySelector('header button')
-expandButton.addEventListener('click', expand)
+const expandButton = document.querySelector("header button");
+expandButton.addEventListener("click", expand);
 
-function expand () {
-  document.body.classList.toggle('expand')
+function expand() {
+  document.body.classList.toggle("expand");
 }
+
+const profileContent = document.getElementById("profile-content");
+
+expandButton.addEventListener("click", () => {
+  profileContent.classList.toggle("hidden");
+});
+
+const button1 = document.getElementById("button1");
+const button2 = document.getElementById("button2");
+const button3 = document.getElementById("button3");
+const infoSection = document.getElementById("profile-info-section");
+const skillsSection = document.getElementById("profile-skills-section");
+const animationSection = document.getElementById("profile-animation-section");
+
+button1.addEventListener("mouseenter", () => {
+  infoSection.style.display = "block";
+  skillsSection.style.display = "none";
+  animationSection.style.display = "none";
+});
+
+button2.addEventListener("mouseenter", () => {
+  infoSection.style.display = "none";
+  skillsSection.style.display = "block";
+  animationSection.style.display = "none";
+});
+
+button3.addEventListener("mouseenter", () => {
+  infoSection.style.display = "none";
+  skillsSection.style.display = "none";
+  animationSection.style.display = "block";
+});
+
